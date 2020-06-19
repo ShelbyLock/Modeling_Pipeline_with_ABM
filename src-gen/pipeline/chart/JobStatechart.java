e4075f158d6ef3e55942615652dc3bac2e90a370
Qianying Liao <liao.sinying@gmail.com>
2020-06-19 10:24:55 +0100

    [tested], the stage handles job based on its capacity.

--- src-gen/pipeline/chart/JobStatechart.java
+++ src-gen/pipeline/chart/JobStatechart.java
@@ -2,6 +2,9 @@
 package pipeline.chart;
 
 import java.util.Map;
+
+import jobs.Job;
+
 import java.util.HashMap;
 
 import repast.simphony.statecharts.*;
@@ -10,26 +13,26 @@ import repast.simphony.statecharts.generator.GeneratedFor;
 import pipeline.*;
 
 @GeneratedFor("_FOTdkKsuEeqdKOUctkVUVA")
-public class JobStatechart extends DefaultStateChart<pipeline.Job> {
+public class JobStatechart extends DefaultStateChart<jobs.Job> {
 
-	public static JobStatechart createStateChart(pipeline.Job agent, double begin) {
+	public static JobStatechart createStateChart(jobs.Job agent, double begin) {
 		JobStatechart result = createStateChart(agent);
 		StateChartScheduler.INSTANCE.scheduleBeginTime(begin, result);
 		return result;
 	}
 
-	public static JobStatechart createStateChart(pipeline.Job agent) {
+	public static JobStatechart createStateChart(jobs.Job agent) {
 		JobStatechartGenerator generator = new JobStatechartGenerator();
 		return generator.build(agent);
 	}
 
-	private JobStatechart(pipeline.Job agent) {
+	private JobStatechart(jobs.Job agent) {
 		super(agent);
 	}
 
-	private static class MyStateChartBuilder extends StateChartBuilder<pipeline.Job> {
+	private static class MyStateChartBuilder extends StateChartBuilder<jobs.Job> {
 
-		public MyStateChartBuilder(pipeline.Job agent, AbstractState<pipeline.Job> entryState, String entryStateUuid) {
+		public MyStateChartBuilder(jobs.Job agent, AbstractState<jobs.Job> entryState, String entryStateUuid) {
 			super(agent, entryState, entryStateUuid);
 			setPriority(0.0);
 		}
--- src/pipeline/PipelineBuilder.java
+++ src/pipeline/PipelineBuilder.java
@@ -4,16 +4,18 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import jobs.Job;
 import repast.simphony.context.Context;
 import repast.simphony.context.space.graph.NetworkBuilder;
 import repast.simphony.context.space.grid.GridFactory;
 import repast.simphony.context.space.grid.GridFactoryFinder;
 import repast.simphony.dataLoader.ContextBuilder;
+import repast.simphony.random.RandomHelper;
 import repast.simphony.space.grid.Grid;
 import repast.simphony.space.grid.GridBuilderParameters;
 import repast.simphony.space.grid.SimpleGridAdder;
 import repast.simphony.space.grid.WrapAroundBorders;
-import stages.Stages;
+import stages.Stage;
 
 public class PipelineBuilder implements ContextBuilder<Object>{
 
@@ -36,62 +38,30 @@ public class PipelineBuilder implements ContextBuilder<Object>{
 		NetworkBuilder<Object> netBuilder = new NetworkBuilder <Object> ("infection network", context , true );
 		netBuilder.buildNetwork ();
 		//*************************************************************************************
-		//get Pipeline: the first argument is the number of pipeline
-		int num_pipelines = 5;
-		int num_stages = 6; 
-		//*************************************************************************************
 		//Build Pipeline
-		int total_num_pipeline = num_pipelines;
-		ArrayList<Stages> sCommit = new ArrayList<Stages>(total_num_pipeline);
-		ArrayList<Stages> sBuild = new ArrayList<Stages>(total_num_pipeline);
-		ArrayList<Stages> sQA = new ArrayList<Stages>(total_num_pipeline);
-		ArrayList<Stages> sUAT = new ArrayList<Stages>(total_num_pipeline);
-		ArrayList<Stages> sSTG = new ArrayList<Stages>(total_num_pipeline);
-		ArrayList<Stages> sProd = new ArrayList<Stages>(total_num_pipeline);
+		int num_stages = 5; 
+		int num_pipelines = 6;
+		Map<Integer, ArrayList<Stage>> pipeline = new HashMap<>(num_stages);
+		for (int j = 1; j <= num_stages; j++) {
+			ArrayList<Stage> sameStageInDifferentPipelines = new ArrayList<Stage>(num_pipelines);
+			for (int i = 1; i <= num_pipelines; i++) {
+				Stage tempStages = new Stage(i,j,grid);
+				sameStageInDifferentPipelines.add(tempStages);
 				
-		int totol_num_stages = num_stages;
-		Map<Integer, ArrayList<Stages>> pipeline = new HashMap<>(totol_num_stages);
-				
-		for (int i = 1; i <= total_num_pipeline; i++) {
-			Stages s1 = new Stages(i,1);
-			Stages s2 = new Stages(i,2);
-			Stages s3 = new Stages(i,3);
-			Stages s4 = new Stages(i,4);
-			Stages s5 = new Stages(i,5);
-			Stages s6 = new Stages(i,6);
-				
-			sCommit.add(s1);
-			sBuild.add(s2);
-			sQA.add(s3);
-			sUAT.add(s4);
-			sSTG.add(s5);
-			sProd.add(s6);
-					
-			context.add(s1);
-			context.add(s2);
-			context.add(s3);
-			context.add(s4);
-			context.add(s5);
-			context.add(s6);
-				
-			grid.moveTo(s1, s1.getX(), s1.getY());
-			grid.moveTo(s2, s2.getX(), s2.getY());
-			grid.moveTo(s3, s3.getX(), s3.getY());
-			grid.moveTo(s4, s4.getX(), s4.getY());
-			grid.moveTo(s5, s5.getX(), s5.getY());
-			grid.moveTo(s6, s6.getX(), s6.getY());
+				context.add(tempStages);
+				grid.moveTo(tempStages, tempStages.getX(), tempStages.getY());
+			}
+			pipeline.put(j, sameStageInDifferentPipelines);
 		}
-		pipeline.put(1, sCommit);
-		pipeline.put(2, sBuild);
-		pipeline.put(3, sQA);
-		pipeline.put(4, sUAT);
-		pipeline.put(5, sSTG);
-		pipeline.put(6, sProd);
-		
+		//*************************************************************************************
+		//Create Jobs -> commit Jobs
 		int jobCount = 100;
 		for (int i = 0; i < jobCount ; i++) {
-			Job new_job = new Job(num_pipelines, num_stages, grid, pipeline);
+			Job new_job = new Job(i,num_pipelines, num_stages, grid, pipeline);
 			context.add(new_job);
+			int pipelineID = RandomHelper.nextIntFromTo (0, num_pipelines - 1);
+			Stage build = pipeline.get(1).get(pipelineID);
+			build.addNewJob(i, new_job);
 		}
 		return context;
 	}
